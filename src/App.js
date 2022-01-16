df6895f6a26c792963fa4b90f034f1d33dea8faf
csg33k <balajiyaswanth.v@gmail.com>
2022-01-17 00:49:21 +0530

    blueprint

--- src/App.js
+++ src/App.js
@@ -3,8 +3,35 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h1>Helloo</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint sit odio nulla et unde nihil suscipit maxime quod eos illo!</p>
+      <div className="temp">
+        <form action="">
+          Name: <br />
+          <input type="text" /> <br />
+          Name: <br />
+          <input type="text" /> <br />
+          Name:<br />
+          <input type="text" /> <br />
+          Name:<br />
+          <input type="text" />
+        </form>
+      </div>
+      <div className="block">
+        {/* Blueprint bish */}
+        <h2>Task 1</h2>
+        <p>Doing dishes &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; x</p>
+
+        <h2>Task 2</h2>
+        <p>Giving my mercedes for servicing &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; x</p>
+
+        <h2>Task 3</h2>
+        <p>Garden work &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; x</p>
+
+        <h2>Task 4</h2>
+        <p>Watering plants &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; x</p>
+
+        <h2>Task 5</h2>
+        <p>Games...? &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; x</p>
+      </div>
     </div>
   );
 }
