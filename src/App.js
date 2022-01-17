3fb09250dfbb13bb3a1c8d7c29718a3dc241dd69
csg33k <balajiyaswanth.v@gmail.com>
2022-01-17 11:35:44 +0530

    Tasks made globally accessible

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import './App.css';
-import React from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [tasks, setTasks] = useState([ /* Default state */
+    {
+      id: 1,
+      chore: "Doing dishes",
+      time: "9AM Sunday",
+      completed: false
+    },
+    {
+      id: 2,
+      chore: "Giving my mercedes for servicing",
+      time: "11AM Sunday",
+      completed: true
+    },
+    {
+      id: 3,
+      chore: "Garden work",
+      time: "4PM Sunday",
+      completed: false
+    },
+    {
+      id: 4,
+      chore: "Watering plants",
+      time: "5PM Sunday",
+      completed: false
+    },
+  ]);
   return (
     <div className="App">
       <Header />
       {/* Blueprint bish */}
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,33 +1,8 @@
 import React from 'react';
 import Task from './Task';
 
-const Tasks = () => {
-  const tasks = [
-    {
-      id: 1,
-      chore: "Doing dishes",
-      time: "9AM Sunday",
-      completed: false
-    },
-    {
-      id: 2,
-      chore: "Giving my mercedes for servicing",
-      time: "11AM Sunday",
-      completed: true
-    },
-    {
-      id: 3,
-      chore: "Garden work",
-      time: "4PM Sunday",
-      completed: false
-    },
-    {
-      id: 4,
-      chore: "Watering plants",
-      time: "5PM Sunday",
-      completed: false
-    },
-  ];
+const Tasks = ({ tasks }) => {
+
     return (
         <div>
           {tasks.map((task) => {
