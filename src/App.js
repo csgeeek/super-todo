2731db01adc7acf63551a8719efc362541a86cfd
csg33k <balajiyaswanth.v@gmail.com>
2022-01-17 09:50:17 +0530

    breaktime

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
 import './App.css';
 import React from 'react';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <div className="temp">
-        <form action="">
-          Name: <br />
-          <input type="text" /> <br />
-          Name: <br />
-          <input type="text" /> <br />
-          Name:<br />
-          <input type="text" /> <br /> <br />
-
-          <input type="Submit" />
-        </form>
-      </div>
-      <div className="block">
-        {/* Blueprint bish */}
-        <h2>Task 1</h2>
-        <p>Doing dishes &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; x</p>
-
-        <h2>Task 2</h2>
-        <p>Giving my mercedes for servicing &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; x</p>
-
-        <h2>Task 3</h2>
-        <p>Garden work &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; x</p>
-
-        <h2>Task 4</h2>
-        <p>Watering plants &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; x</p>
-
-        <h2>Task 5</h2>
-        <p>Games...? &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; x</p>
-      </div>
+      {/* Blueprint bish */}
+      <Tasks />
     </div>
   );
 }
